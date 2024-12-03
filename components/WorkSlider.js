e816ddcc0251aacca9dca29eb51553d8ac5eecf0
Prince Rathod <rathodprince411@gmail.com>
2024-12-03 18:58:57 +0530

    indentations applied no change in code

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -60,32 +60,32 @@ const WorkSlider = () => {
           {slide.images.map((image, index) => {
             return <Link key={index} href={image.Link} target="_blank" >
               <div className="relative rounded-lg overflow-hidden flex items-center justify-center group " >
-            <div className="flex items-center justify-center relative overflow-hidden group " key={index} >
-              {/* image  */}
-              <Image src={image.path} width={500} height={300} alt="" />
-              {/* overlay gradient */}
-              
-                <div className=" absolute inset-0 bg-gradient-to-l from-transparent via-accent to-transparent opacity-0 group-hover:opacity-80 transition-all duration-700 " ></div>
-                {/* title  */}
-                <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300 " >
-                  <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]" >
-                    {/* title part 1 */}
-                    <div className="delay-100 " >
-                    GitHub
+                <div className="flex items-center justify-center relative overflow-hidden group " key={index} >
+                  {/* image  */}
+                  <Image src={image.path} width={500} height={300} alt="" />
+                  {/* overlay gradient */}
+                  
+                    <div className=" absolute inset-0 bg-gradient-to-l from-transparent via-accent to-transparent opacity-0 group-hover:opacity-80 transition-all duration-700 " ></div>
+                    {/* title  */}
+                    <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300 " >
+                      <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]" >
+                        {/* title part 1 */}
+                        <div className="delay-100 " >
+                        GitHub
+                        </div>
+                        {/* title part 2 */}
+                        <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150 " >
+                        Link
+                        </div>
+                        {/*icon */}
+                        <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200 " >
+                        <BsArrowRight />
+                        </div>
+                      </div>
                     </div>
-                    {/* title part 2 */}
-                    <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150 " >
-                    Link
-                    </div>
-                    {/*icon */}
-                    <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200 " >
-                    <BsArrowRight />
-                    </div>
-                  </div>
+                  
                 </div>
-              
-            </div>
-          </div>
+             </div>
           </Link>
           })}
         </div>
