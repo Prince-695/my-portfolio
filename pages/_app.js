1eb3fadc111a7f13b6ab7afa9c5d6b119d12fc9a
Prince Rathod <rathodprince411@gmail.com>
2025-12-24 15:21:00 +0530

    analytics update check

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,12 @@ import Layout from '../components/Layout';
 import Transition from '../components/Transition';
 import { useRouter } from 'next/router';
 import { AnimatePresence, motion } from 'framer-motion'; 
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return(
-    <Layout>
+    <>
       <AnimatePresence mode='wait'>
         <motion.div key={router.route} className='h-full' >
           <Transition />
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }) {
         </motion.div>
       </AnimatePresence>
       <Analytics />
-    </Layout>
+    </>
   )
   
 }
